02f52bfc4b063f2988aa26ed8b016e96ddc3dcbb
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-07 17:39:20 +0200

    [UPD] Finishing Milestone 3, added the main logic and fixing some css and js

--- script.js
+++ script.js
@@ -3,35 +3,22 @@ const endpoint= 'https://lanciweb.github.io/demo/api/pictures/'
 
 
 // funzione per creare le card, all'interno genero la chiamata AJAX
-
 const picturesCard =()=>{
     axios.get(endpoint).then(res=>{
      const details= res.data; // ottengo i dati dell'elemento (array di oggetti)
      
-        // const array = details.filter((item) =>(item.title))
-
-//         const card =   ` <div class="col-12 col-md-6 col-lg-4">
-//                     <div class="card mt-5 pt-3 px-3">
-//                         <img src="${details.url}" class="img-fluid" alt="">
-//                         <img src="./img/pin.svg" class="pin-rosso" alt="">
-//                         <span class="card-text py-1 text-center">${details.title} ${details.date}</span>
-//                     </div>
-//                 </div>`;
-
-// return card;
-//     })
+     let cards= ''; // creo una stringa vuota in cui concatenerò i nuovi elementi
+     details.forEach(item =>{
+                const card =   ` <div class="col-12 col-md-6 col-lg-4">
+                            <div class="card mt-5 pt-3 px-3">
+                                <img src="${item.url}" class="img-fluid" alt="">
+                                <img src="./img/pin.svg" class="pin-rosso" alt="">
+                                <span class="card-text py-1 text-center">${item.title} ${item.date}</span>
+                            </div>
+                                </div>`;
+                cards += card;
+                document.getElementById('card-pictures').innerHTML= cards;
+                })
+     })
 }
     picturesCard()
-
-
-// altra parte (provvisoria)
-// const renderCard= ()=>{
-
-// let allCards= "";
-
-// for(let i= 0; i<details.length; i++){
-//     allCards += picturesCard(details[i])
-// }
-// document.getElementById('card-pictures').innerHTML= allCards;
-// }
-// renderCard();
